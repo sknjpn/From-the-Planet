d56eed80a6df30a8eb54bd1b622f4537bd017196
SKNJPN <sknjpn@gmail.com>
2019-10-19 22:56:27 +0900

    Stateにアクセスできるように

--- FacilityAsset.h
+++ FacilityAsset.h
@@ -7,7 +7,12 @@ class FacilityState;
 class FacilityAsset :
 	public Asset
 {
+	shared_ptr<FacilityState>	m_facilityState;
+
 public:
 	virtual shared_ptr<FacilityState>	makeState() = 0;
+
+	template<typename T>
+	shared_ptr<T>	getFacilityState() const { return dynamic_pointer_cast<T>(m_facilityState); }
 };
 
