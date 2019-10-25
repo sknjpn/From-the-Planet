4528de0e5866cbc2bbd602465fbe4dbb714941a5
SKNJPN <sknjpn@gmail.com>
2019-10-25 16:10:57 +0900

    メンバのconst化

--- Road.h
+++ Road.h
@@ -4,10 +4,10 @@ class Region;
 
 class Road
 {
-public:
 	weak_ptr<Region> m_fr;
 	weak_ptr<Region> m_to;
 
+public:
 	Road() {}
 	Road(const shared_ptr<Region> fr, const shared_ptr<Region> to)
 		: m_fr(fr)
