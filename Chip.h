4f16e6421ee812e1d6d83e7f2e3ea822ec229f04
SKNJPN <sknjpn@gmail.com>
2019-10-25 16:20:46 +0900

    権限の修正

--- Chip.h
+++ Chip.h
@@ -6,15 +6,19 @@ class Chip
 {
 	friend class PlanetManager;
 
-public:
 	shared_ptr<Region>	m_r0;
 	shared_ptr<Region>	m_r1;
 	shared_ptr<Region>	m_r2;
 	Vec3	m_circumcenter;
 	Vec3	m_center;
 
+public:
+
 	Chip(const shared_ptr<Region>& r0, const shared_ptr<Region>& r1, const shared_ptr<Region>& r2);
 
 	void	draw(const Mat4x4& mat, const Color& color);
 	Array<Vec3>	getPositions() const;
+
+	const Vec3& getCircumcenter() const { return m_circumcenter; }
+	const Vec3& getCenter() const { return m_center; }
 };
