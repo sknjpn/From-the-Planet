f0e01890becf24ae7ac9aa7ea6bfe0ae40e35670
SKNJPN <sknjpn@gmail.com>
2019-11-07 21:25:48 +0900

    SyLifeのViewer移植

---
+++ Viewer.cpp
@@ -0,0 +1,94 @@
+﻿#include "Viewer.h"
+
+shared_ptr<Viewer>	Viewer::g_mouseoveredViewer;
+
+void Viewer::UpdateAllViewers()
+{
+	// Viewerのリセット
+	{
+		auto viewers = GetRootViewer()->getAllChildViewers();
+		g_mouseoveredViewer = nullptr;
+		for (auto it = viewers.begin(); it < viewers.end(); ++it)
+		{
+			(*it)->m_drawPos = Vec2::Zero();
+
+			if ((*it)->m_viewerRect.mouseOver()) g_mouseoveredViewer = *it;
+		}
+
+		// Viewer 更新
+		for (auto it = viewers.begin(); it < viewers.end(); ++it)
+		{
+			// 消されたならば処理しない
+			if ((*it)->m_isDestroyed) continue;
+
+			auto vp = ScopedViewport2D(Rect((*it)->m_viewerRect));
+			auto t = Transformer2D(Mat3x2::Identity(), Mat3x2::Translate((*it)->m_viewerRect.pos));
+
+			RectF((*it)->m_viewerRect.size).draw((*it)->m_backgroundColor);
+
+			(*it)->update();
+
+			(*it)->m_transformer.reset();
+
+			// フレーム描画
+			if (KeyF.pressed()) RectF((*it)->m_viewerRect.size).drawFrame(1.0, 0.0, ColorF(Palette::Red, 0.5));
+		}
+	}
+
+	// destroyされたViewerの削除
+	for (;;)
+	{
+		auto viewers = GetRootViewer()->getAllChildViewers();
+		bool flag = true;
+
+		for (auto& v : viewers)
+		{
+			if (v->m_childViewers.isEmpty() && v->m_isDestroyed)
+			{
+				v->m_parentViewer->m_childViewers.remove(v);
+
+				flag = false;
+			}
+		}
+
+		if (flag) break;
+	}
+}
+
+void Viewer::Run()
+{
+	while (System::Update())
+	{
+		// ログ出力のクリア
+		ClearPrint();
+
+		UpdateAllViewers();
+	}
+}
+
+const shared_ptr<Viewer>& Viewer::GetRootViewer()
+{
+	static shared_ptr<Viewer> rootViewer = MakeShared<Viewer>();
+
+	return rootViewer;
+}
+
+void Viewer::destroy()
+{
+	m_isDestroyed = true;
+
+	for (auto& cv : m_childViewers)
+		cv->destroy();
+}
+
+Array<shared_ptr<Viewer>> Viewer::getAllChildViewers() const
+{
+	Array<shared_ptr<Viewer>> result;
+
+	result.append(m_childViewers);
+
+	for (const auto& cv : m_childViewers)
+		result.append(cv->getAllChildViewers());
+
+	return result;
+}
--- Viewer.h
+++ Viewer.h
@@ -1,46 +1,98 @@
 ﻿#pragma once
 
 class Viewer
+	: public enable_shared_from_this<Viewer>
 {
 	friend class ViewerManager;
 
-	bool	m_isInvisible = false;
-	bool	m_isMouseOver = false;
-	int		m_priority = 0;
+	bool	m_isRoot = false;
+	bool	m_isDestroyed = false;
 	Vec2	m_drawPos = Vec2::Zero();
-	RectF	m_drawRect = RectF(Scene::Size());
-	Color	m_backgroundColor = Color(11, 22, 33, 128);
+	RectF	m_viewerRect = RectF(Scene::Size());
+	Color	m_backgroundColor = Color(0, 0);
 	unique_ptr<Transformer2D>	m_transformer;
+	shared_ptr<Viewer>			m_parentViewer;
+	Array<shared_ptr<Viewer>>	m_childViewers;
+	static shared_ptr<Viewer>	g_mouseoveredViewer;
+
+	static void UpdateAllViewers();
 
 public:
 	virtual ~Viewer() = default;
 
+	static void Run();
+	static const shared_ptr<Viewer>& GetRootViewer();
+
+	const shared_ptr<Viewer>& getParentViewer() const { return m_parentViewer; }
+
+	template <typename T>
+	shared_ptr<T>	getParentViewer() const
+	{
+		if (dynamic_pointer_cast<T>(m_parentViewer) != nullptr) return dynamic_pointer_cast<T>(m_parentViewer);
+
+		return nullptr;
+	}
+
+	template <typename T>
+	shared_ptr<T>	getChildViewer() const
+	{
+		for (auto it = m_childViewers.begin(); it != m_childViewers.end(); ++it)
+			if (dynamic_pointer_cast<T>(*it) != nullptr) return dynamic_pointer_cast<T>(*it);
+
+		return nullptr;
+	}
+
+	template <typename T>
+	bool	hasChildViewer() const
+	{
+		for (auto it = m_childViewers.begin(); it != m_childViewers.end(); ++it)
+			if (dynamic_pointer_cast<T>(*it) != nullptr) return true;
+
+		return false;
+	}
+
+	template <typename T, typename... Args>
+	shared_ptr<T>	addChildViewer(Args&&... args)
+	{
+		auto cv = make_shared<T>(args...);
+
+		m_childViewers.emplace_back(cv);
+		cv->m_parentViewer = shared_from_this();
+		cv->init();
+
+		return cv;
+	}
+
+	void	destroy();
+
+	const Array<shared_ptr<Viewer>>& getChildViewers() const { return m_childViewers; }
+
+	// 再帰的
+	Array<shared_ptr<Viewer>>	getAllChildViewers() const;
+
 	// Set
-	void	setInvisible(bool isInvisible) { m_isInvisible = isInvisible; };
-	void	setPriority(int priority) { m_priority = priority; }
 	void	setBackgroundColor(const Color& color) { m_backgroundColor = color; }
 	void	setDrawPos(const Vec2& pos) { m_drawPos = pos; m_transformer.reset(); m_transformer = make_unique<Transformer2D>(Mat3x2::Translate(m_drawPos), true); }
 	void	setDrawPos(double x, double y) { setDrawPos(Vec2(x, y)); }
-	void	setDrawSize(const Vec2& size) { m_drawRect.size = size; }
-	void	setDrawSize(double x, double y) { setDrawSize(Vec2(x, y)); }
-	void	setDrawRect(const Vec2& pos, const Vec2& size) { m_drawRect = RectF(pos, size); }
-	void	setDrawRect(const Rect& rect) { m_drawRect = rect; }
-	void	setDrawRect(const RectF& rect) { m_drawRect = rect; }
-	void	setDrawRect(const Vec2& size) { m_drawRect = RectF(size); }
-	void	setDrawRect(double x, double y, double w, double h) { m_drawRect = RectF(x, y, w, h); }
-	void	setDrawRect(double w, double h) { m_drawRect = RectF(w, h); }
+	void	setViewerSize(const Vec2& size) { m_viewerRect.size = size; }
+	void	setViewerSize(double x, double y) { setViewerSize(Vec2(x, y)); }
+	void	setViewerPos(const Vec2& pos) { m_viewerRect.pos = pos; }
+	void	setViewerPos(double x, double y) { setViewerPos(Vec2(x, y)); }
+	void	setViewerRect(const Vec2& pos, const Vec2& size) { m_viewerRect = RectF(pos, size); }
+	void	setViewerRect(const Rect& rect) { m_viewerRect = rect; }
+	void	setViewerRect(const RectF& rect) { m_viewerRect = rect; }
+	void	setViewerRect(const Vec2& size) { m_viewerRect = RectF(size); }
+	void	setViewerRect(double x, double y, double w, double h) { m_viewerRect = RectF(x, y, w, h); }
+	void	setViewerRect(double w, double h) { m_viewerRect = RectF(w, h); }
 	void	moveDrawPos(double dx, double dy) { setDrawPos(m_drawPos.movedBy(dx, dy)); }
 
 	// Get
-	int		getPriority() const { return m_priority; }
-	bool	isInvisible() const { return m_isInvisible; }
-	bool	isMouseOver() const { return m_isMouseOver; }
-	const RectF&	getDrawRect() const { return m_drawRect; }
-	const Vec2&		getDrawSize() const { return m_drawRect.size; }
-	const Vec2		getDrawCenter() const { return m_drawRect.center(); }
-	const Vec2&		getDrawPos() const { return m_drawPos; }
+	bool	isMouseover() const { return shared_from_this() == g_mouseoveredViewer; }
+	const RectF& getViewerRect() const { return m_viewerRect; }
+	const Vec2& getViewerSize() const { return m_viewerRect.size; }
+	const Vec2& getDrawPos() const { return m_drawPos; }
+	const Vec2	getDrawCenter() const { return m_viewerRect.center(); }
 
 	virtual void	init() {}
 	virtual void	update() {}
 };
-
