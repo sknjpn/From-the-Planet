9c089e88afc10c64db5d221a77a859f778e5890d
SKNJPN <sknjpn@gmail.com>
2019-11-07 22:48:13 +0900

    TerrainAssetの追加

---
+++ TerrainAsset.cpp
@@ -0,0 +1,8 @@
+﻿#include "stdafx.h"
+#include "TerrainAsset.h"
+
+void TerrainAsset::load(const JSONValue& json)
+{
+	m_color = json[U"color"].get<Color>();
+	m_buildAvailable = json[U"buildAvailable"].get<bool>();
+}
